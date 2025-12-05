778d6517c307a1f1098e4d8eb319cfb813c1fc53
hf4137 <hf4137@wayne.edu>
2025-12-05 15:20:13 -0500

    Update tt_um_hf4137_2_4_decoder.v

--- src/tt_um_hf4137_2_4_decoder.v
+++ src/tt_um_hf4137_2_4_decoder.v
@@ -38,7 +38,6 @@ module tt_um_hf4137_2_4_decoder (
     assign uo_out[7] = 1'b0; 
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
